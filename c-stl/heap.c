2d4919392f48154362457be85d8074f54c68558f
heluan <heluan123132@163.com>
2018-01-23 17:20:51 +0800

    完成通用堆的相关代码

--- c-stl/heap.c
+++ c-stl/heap.c
@@ -1,30 +1,112 @@
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
 #include "heap.h"
 
 //初始化堆
-void heap_init(heap *heap, uint size)
+int heap_init(heap *heap, uint size)
 {
+	//参数检查
+	assert(heap);
+	if (!heap) return HEAP_PARAM_ERROR;
+
+	//申请内存
+	int len = size * sizeof(heap_item *);
+	heap->table = (heap_item **)malloc(len);
+	assert(heap->table != NULL);
+	if (!heap->table) return HEAP_MEM_ERROR;
+
+	//初始化值
+	heap->size = size;
+	heap->count = 0;
+	bzero(heap->table, len);
+
+	return HEAP_SUCCESS;
 }
 
 //加入元素
 int heap_push(heap *heap, heap_item *item)
 {
-	return 0;
+	//参数检查
+	assert(heap != NULL && item != NULL);
+	if (NULL == heap || NULL == item)
+	{
+		return HEAP_PARAM_ERROR;
+	}
+
+	//如果满了，扩展内存。
+	if (heap_full(heap))
+	{
+		int new_size = heap->size + heap->size / 2;
+		heap_item **new_table = (heap_item **)realloc(heap->table, 
+								new_size * sizeof(heap_item *));
+		if (!new_table) return HEAP_MEM_ERROR;
+
+		heap->table = new_table;
+		heap->size = new_size;
+	}
+
+	//添加数据
+	heap->table[++heap->count] = item;
+
+	return heap_up(heap, heap->count);
 }
 
 //弹出元素
 heap_item *heap_pop(heap *heap)
 {
-	return 0;
+	heap_item *temp;
+	swap(heap->table[1], heap->table[heap->count - 1], temp);
+	heap->count--;
+	heap_down(heap, 1);
+
+	return temp;
 }
 
 //上浮
-int heap_up(heap *heap, int i)
+int heap_up(heap *heap, uint i)
 {
-	return 0;
+	//参数检查
+	assert(heap && i > 0);
+	if (!heap || i == 0) return HEAP_PARAM_ERROR; 
+
+	heap_item *temp;
+
+	while (i > 1)
+	{
+		if (heap->table[i]->key cmp heap->table[i >> 1]->key)
+			swap(heap->table[i], heap->table[i >> 1], temp);
+
+		i >>= 1;
+	}
+
+	return HEAP_SUCCESS;
 }
 
 //下沉
-int heap_down(heap *heap, int i)
+int heap_down(heap *heap, uint i)
 {
-	return 0;
+	//参数检查
+	assert(heap && i > 0);
+	if (!heap || i == 0) return HEAP_PARAM_ERROR; 
+
+	uint next;
+	heap_item *temp;
+
+	while (i << 1 <= heap->count)
+	{
+		next = i << 1;
+		if (heap->table[next + 1]->key cmp heap->table[next]->key)
+			++next;
+
+		if (heap->table[next]->key cmp heap->table[i]->key)
+		{
+			swap(heap->table[next], heap->table[i], temp);
+			i = next;
+		}
+		else
+			return HEAP_SUCCESS;
+	}
+
+	return HEAP_SUCCESS;
 }
--- c-stl/heap.h
+++ c-stl/heap.h
@@ -3,12 +3,17 @@
 
 #include <sys/types.h>
 
+#define			HEAP_SUCCESS		0							//成功
+#define			HEAP_FAILURE		-1							//失败
+#define			HEAP_PARAM_ERROR	-2							//参数错误
+#define			HEAP_MEM_ERROR		-3							//参数错误
 
 #define			cmp					<							//大顶堆：>; 小顶堆：<
 #define 		as_heap_item		heap_item	heap_item_		//堆元素头
 
-#define			heap_top(heap)		(heap)->array[0]			//取得顶元素
-#define			heap_empty(heap)	((heap)->count == 0)		//是否为空
+#define			heap_top(heap)		(heap)->table[1]					//取得顶元素
+#define			heap_empty(heap)	((heap)->count == 0)				//是否为空
+#define			heap_full(heap)		((heap)->count + 1 == (heap)->size)	//是否已满
 
 //堆元素头
 typedef struct _heap_item
@@ -21,18 +26,18 @@ typedef struct _heap
 {
 	uint			size;			//大小
 	uint			count;			//堆中元素个数
-	heap_item		**array;		//存储堆元素的数组
+	heap_item		**table;		//存储堆元素的数组
 }heap;
 
 //初始化堆
-void heap_init(heap *heap, uint size);
+int heap_init(heap *heap, uint size);
 //加入元素
 int heap_push(heap *heap, heap_item *item);
 //弹出元素
 heap_item *heap_pop(heap *heap);
 //上浮
-int heap_up(heap *heap, int i);
+int heap_up(heap *heap, uint i);
 //下沉
-int heap_down(heap *heap, int i);
+int heap_down(heap *heap, uint i);
 
 #endif //_HEAP_H_
